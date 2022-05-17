fe232d0efe8600ee353602e7ab5f82c3f6f47926
davidemarcoli <davide@marcoli.ch>
2022-05-17 15:55:11 +0200

    Fix unnecessary quotes

--- control/register.php
+++ control/register.php
@@ -5,9 +5,9 @@
 include_once '../includes/dbconnection.php';
 
 if (isset($_POST)) {
-    $username = $GLOBALS['con']->quote($_POST['username']);
-    $email = $GLOBALS['con']->quote($_POST['email']);
-    $password = $GLOBALS['con']->quote($_POST['password']);
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
     $password = password_hash($password, PASSWORD_BCRYPT);
     $stmt = $dbh->prepare("INSERT INTO app_user (username, email, password) VALUES (:username, :email, :password)");
     $stmt->bindParam(':username', $username);
