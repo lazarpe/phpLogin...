92f0d395282567b29b561e864630f50b6b522d54
davidemarcoli <davide@marcoli.ch>
2022-05-17 15:16:28 +0200

    Add Hash Helper File

--- control/login.php
+++ control/login.php
@@ -4,7 +4,7 @@ include_once 'includes/dbconnection.php';
 if (isset($_POST)) {
     $email = $GLOBALS['con'].quote($_POST['email']);
     $password = $GLOBALS['con'].quote($_POST['password']);
-    $password = password_hash($password);
+    $password = password_hash($password, PASSWORD_BCRYPT);
     $sql = "SELECT * FROM app_user WHERE email = '$email' AND password = '$password'";
     $result = $con->query($sql);
     if ($result->rowCount() > 0) {
